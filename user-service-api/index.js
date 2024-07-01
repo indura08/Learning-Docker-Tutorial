a4edf796eb56ef0455871a88d2faffa12dc32288
indura08 <induraperera3@gmail.com>
2024-07-01 16:41:26 +0530

    first commit

---
+++ user-service-api/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+
+app.get('/' , (req ,res ) => res.json([
+    {
+        name:"indura",
+        city:"makola"
+    },
+
+    {
+        name:"deepa",
+        city:"kribathgoda"
+    },
+    {
+        name:"sampath",
+        city: "kotikawatta"
+    }
+]))
+
+app.listen(5000, () => console.log("app is listening on port 3000"));
